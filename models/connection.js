69c97dfd40081d998d6c1ac39c66b0f067870690
Ousmane Diarra <diarraousmane37@gmail.com>
2024-12-28 23:44:14 +0100

    routes users
    routes signin, signup, delete, logout, get user, update user, update role & update password backend + auth + tests

--- models/connection.js
+++ models/connection.js
@@ -3,24 +3,24 @@ const { createDatabase } = require("../modules/database_creation");
 require("dotenv").config();
 
 var con = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
-    multipleStatements: true,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+  multipleStatements: true,
 });
 
 con.connect(function (err) {
-    if (err) {
-        console.error(
-            "❌ A problem occured while trying to establish mysql connection",
-            err
-        );
-        return { result: false };
-    }
-    console.log("✅ Mysql Database connected");
-    createDatabase(con);
+  if (err) {
+    console.error(
+      "❌ A problem occured while trying to establish mysql connection",
+      err
+    );
+    return { result: false };
+  }
+  console.log("✅ Mysql Database connected");
+  // createDatabase(con);
 });
 
 module.exports = con;
