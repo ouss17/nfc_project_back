b2fd36678fc273a1494a4815faf6f626e038ae5b
Ousmane Diarra <mr.ousmanediarra@gmail.com>
2025-02-02 18:43:46 +0100

    enable connection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 
-// require("./models/connection");
+require("./models/connection");
 
 var indexRouter = require("./routes/index");
 // var usersRouter = require("./routes/users");
