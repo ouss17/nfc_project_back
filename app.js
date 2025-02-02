33ac0efca6f2737cf23eeacf02ce002bc23f9cde
Ousmane Diarra <mr.ousmanediarra@gmail.com>
2025-02-02 18:40:18 +0100

    try without bdd

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
 
-require("./models/connection");
+// require("./models/connection");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var historicRouter = require("./routes/paiementsHistoric");
-var priceRouter = require("./routes/availablePrices");
-var stripeRouter = require("./routes/stripe");
+// var usersRouter = require("./routes/users");
+// var historicRouter = require("./routes/paiementsHistoric");
+// var priceRouter = require("./routes/availablePrices");
+// var stripeRouter = require("./routes/stripe");
 
 var app = express();
 
@@ -27,9 +27,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/historic", historicRouter);
-app.use("/prices", priceRouter);
-app.use("/stripe", stripeRouter);
+// app.use("/users", usersRouter);
+// app.use("/historic", historicRouter);
+// app.use("/prices", priceRouter);
+// app.use("/stripe", stripeRouter);
 
 module.exports = app;
